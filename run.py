2891c4cea9bcc444f1134c045a836e9778ad30f0
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-02-21 12:37:47 +0300

    run to input first name

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.8
+
+import os
+from credentials import Credential
+from user import User
+import random
+
+
+def main():
+    print("Hi,Hello!! Welcome password_locker. Enter User First Name.")
+    first_name = input()
