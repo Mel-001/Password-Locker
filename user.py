0d07e4f13643d851548ea95a5703f2e7a02f32e8
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-02-21 12:21:16 +0300

    init function to initialize user credentials

--- user.py
+++ user.py
@@ -4,3 +4,15 @@ class User(Credential):
     """
     Class that generates a new instance User
     """
+
+    def __init__(self, first_name, last_name,  user_email, user_password):
+        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_email = user_email
+        self.user_password = user_password
+        self.isLoggedIn = True
+
+
+
+
