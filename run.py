15f00aca0fe5488acdea958d27d583ba7da4c240
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-02-21 12:38:32 +0300

    run to input last name

--- run.py
+++ run.py
@@ -9,3 +9,6 @@ import random
 def main():
     print("Hi,Hello!! Welcome password_locker. Enter User First Name.")
     first_name = input()
+
+    print(f"Hey {first_name} you're a step into password_locker. Please enter your last name.")
+    last_name = input()
